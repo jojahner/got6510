810c35135a9d302c300c16ba4f00cdb427a26c90
Jan-Oliver Jahner <jojahner@gmail.com>
2019-05-20 14:46:43 +0200

    Print world

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 all: bin/fly.prg
 
 bin/fly.prg: src/main.asm
-	acme -v3 -f cbm -o bin/fly.bin src/main.asm
+	acme -v3 -f cbm -vl bin/fly.labels -o bin/fly.bin src/main.asm
 	./tools/pucrunch -x4096 bin/fly.bin bin/fly.prg
 
 run: bin/fly.prg
 	x64 bin/fly.prg
 
 clean:
-	rm bin/fly.bin
-	rm bin/fly.prg
+	rm bin/*.labels
+	rm bin/*.bin
+	rm bin/*.prg
--- src/main.asm
+++ src/main.asm
@@ -1,30 +1,64 @@
+; * = $0801                                       ; BASIC start address (#2049)
+; !byte $0d,$08,$dc,$07,$9e,$20,$34,$39           ; BASIC loader to start at $c000...
+; !byte $31,$35,$32,$00,$00,$00                   ; BASIC op-codes to execute 'SYS 49152'  (49152 = 0xc000)
+
+; * = $c000
+
 ; *=$0800
 ; !byte $00,$0c,$08,$0a,$00,$9e,$32,$30,$36,$33,$00,$00,$00,$00
 ; *=$080f
 
 *=$1000
 
+
 start
-    ; Make screen black and text white
-    lda #$00
-    sta $d020
-    sta $d021
-    lda #$01
-    sta $0286
-    ; Clear the screen and jump to draw routine
+    ; Set background and border to black
+    lda #$00    ; Black
+    sta $d020   ; Set border color
+    sta $d021   ; Set background color
+    ; Set cursor colot to orange
+    lda #$08    ; Orange
+    sta $0286   ; Set cursor color
+
+    ; Clear the screen
     jsr $e544
-    jsr draw_text
-    jmp *
+    jsr draw_world
 
-message
-    !scr "              hello world!!             "	; 40 cols of text
+    jmp *       ; loop forever
 
-draw_text
-    ldx #$00         ; init X-Register with $00
-loop_text
-    lda message,x    ; read characters from message text...
-    sta $0590,x      ; ...and place it into screen screen memory
-    inx 				; increment to next character
-    cpx #$28         ; false if != 40
-    bne loop_text    ; loop if false
+
+screen_pointer = $02
+
+world !fill 1000, $00 ; 1 byte per cell, each bit represents a neighbour
+
+draw_world
+    ; init screen pointer
+    lda #$00
+    sta screen_pointer
+    lda #$04
+    sta screen_pointer+1
+
+    lda #$FF ; $a0
+    ldx #$00
+
+_draw_world_loop
+    ldy #$00
+
+    ; test
+    adc #$01
+    and #$7F
+
+_draw_world_line
+    sta (screen_pointer), Y
+    iny
+    cpy #$e8
+    bne _draw_world_line
+
+
+    inc screen_pointer+1
+
+    inx
+    cpx #$04
+    bne _draw_world_loop
+debug:
     rts
